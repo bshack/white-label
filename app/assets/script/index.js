8f6dc9526779088092f0db59f90c249611cff087
Bill Shackelford <billshackelford@Bills-MacBook-Pro.local>
2016-06-05 23:09:24 -0400

    fix service config

--- app/assets/script/index.js
+++ app/assets/script/index.js
@@ -4,18 +4,4 @@ import modelGlobal from './model/global';
 
 (function() {
     'use strict';
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-            if(xmlhttp.status == 200){
-                window.console.log(JSON.parse(xmlhttp.responseText));
-            } else if(xmlhttp.status == 400) {
-                window.console.log('400 error');
-            } else {
-                window.console.log('error');
-            }
-        }
-    };
-    xmlhttp.open('GET', modelGlobal.get().www + 'assets/data/config.json', true);
-    xmlhttp.send();
 })();
