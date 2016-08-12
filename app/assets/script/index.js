39a7a9c0eba0a35056ee9d8f7bc0211e42f67d6d
Bill Shackelford <billshackelford@Bills-MacBook-Pro.local>
2016-08-12 16:00:08 -0400

    v2.2.19

--- app/assets/script/index.js
+++ app/assets/script/index.js
@@ -1,11 +1,7 @@
 import 'babel-polyfill';
 import modernizr from 'modernizr';
 import modelGlobal from './model/singletons/global';
-import template from './template/element/a';
 
 (function() {
     'use strict';
-    window.console.log(template({
-        title: 'foo'
-    }));
 })();
