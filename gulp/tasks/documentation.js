fdbafbcb178aad82f968d12c32d8bd4db2089048
William Shackelford <williamshackelford@Williams-MacBook-Pro.local>
2016-11-21 23:26:51 -0500

    add in js doc

--- gulp/tasks/documentation.js
+++ gulp/tasks/documentation.js
@@ -3,10 +3,24 @@
 // ## Load Modules
 
 const gulp = require('gulp');
+const jsdoc = require('gulp-jsdoc3');
+
+// ## Environment Config
+
+const config = require('../config');
+const jsdocConfig = require('../../jsdoc');
+
+// ## jsdoc Task
+// runs jsdoc
+gulp.task('jsdoc', (callback) => {
+    gulp.src(config.path.script.all, {read: false})
+        .pipe(jsdoc(jsdocConfig, callback));
+});
 
 // ## Documenation Task
-// documentation task compiles all docco generated documents into handlbars templates
+// documentation task runs unit testing coverage and also js documentation
 gulp.task('documentation', [
+    'jsdoc',
     'unit'
 ], () => {
     'use strict';
