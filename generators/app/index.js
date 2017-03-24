f374286f2f290d78eafa954d9e9fff4b4dad079e
Bill Shackelford <bshackelford@rgbs-mac-pro.rgbarry.local>
2017-03-24 09:59:00 -0400

    update support for yeoman

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,43 +1,43 @@
 'use strict';
 
 // ## Load Modules
-const yeoman = require('yeoman-generator');
+const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 
 // ## Setup
-module.exports = yeoman.Base.extend({
-    prompting: function() {
+module.exports = class extends Generator {
+    prompting() {
         this.log(yosay(
             'Setting up ' + chalk.red('generator-shackstack') + ' generator.'
         ));
-    },
-    writing: function() {
+    }
+    writing() {
         //copy over the site files
         this.fs.copy(
-          this.templatePath('../../../app/**'),
-          this.destinationPath('./app')
+            this.templatePath('../../../app/**'),
+            this.destinationPath('./app')
         );
         //copy over the tasks
         this.fs.copy(
-          this.templatePath('../../../gulp/**'),
-          this.destinationPath('./gulp')
+            this.templatePath('../../../gulp/**'),
+            this.destinationPath('./gulp')
         );
         //copy over the root files
         this.fs.copy(
-          this.templatePath('../../../*.*'),
-          this.destinationPath('./')
+            this.templatePath('../../../*.*'),
+            this.destinationPath('./')
         );
         //copy over hidden files
         this.fs.copy(
-          this.templatePath('../../../.*'),
-          this.destinationPath('./')
+            this.templatePath('../../../.*'),
+            this.destinationPath('./')
         );
-    },
-    install: function() {
+    }
+    install() {
         this.installDependencies({
             bower: false,
             npm: true
         });
     }
-});
+};
