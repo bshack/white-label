194b72e0add7a9c908bafad45dea9119b68b6c91
William Shackelford <bshackelford@gmail.com>
2026-09-14 10:26:35 -0400

    Update generator test for corrected View pin

--- test/generator.test.js
+++ test/generator.test.js
@@ -124,7 +124,7 @@ test('packaged project creator creates a strictly typed Tailwind and JSX site th
     assert.equal(manifest.dependencies['white-label-mediator'], 'github:bshack/white-label-mediator#e815f704a759de76f96f66ac198b4dc42dfc30f4');
     assert.equal(manifest.dependencies['white-label-model'], 'github:bshack/white-label-model#b5b45b13b45f509497d0a5bdbed86c8bcc054980');
     assert.equal(manifest.dependencies['white-label-router'], 'github:bshack/white-label-router#23295657a29764a140218e03d384631ce3a2b9c3');
-    assert.equal(manifest.dependencies['white-label-view'], 'github:bshack/white-label-view#b612930edaef814412899b3391a14fd36df28d9d');
+    assert.equal(manifest.dependencies['white-label-view'], 'github:bshack/white-label-view#831165d4ea6d94c6a96f082f3d43bbed85cdb3f7');
     assert.equal(manifest.dependencies.eta, undefined);
     assert.equal(manifest.devDependencies.bootstrap, undefined);
     assert.equal(manifest.devDependencies.sass, undefined);
